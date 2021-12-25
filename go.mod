d11b6c7ba1abe82fc95b069cd596185dcff891ab
sakurai-ryo <sakurairyo.10do@gmail.com>
2021-12-25 13:47:06 +0900

    [add] request log

--- go.mod
+++ go.mod
@@ -5,6 +5,10 @@ go 1.17
 require (
 	github.com/cornelk/hashmap v1.0.1
 	github.com/gorilla/mux v1.8.0
+	github.com/sirupsen/logrus v1.8.1
 )
 
-require github.com/dchest/siphash v1.1.0 // indirect
+require (
+	github.com/dchest/siphash v1.1.0 // indirect
+	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
+)
--- router.go
+++ router.go
@@ -1,9 +1,26 @@
 package main
 
-import "github.com/gorilla/mux"
+import (
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+	"net/http"
+)
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		logrus.WithFields(logrus.Fields{
+			"method": r.Method,
+			"host":   r.Host,
+			"path":   r.URL.Path,
+			"Ua":     r.UserAgent,
+		}).Info("incoming request")
+		next.ServeHTTP(w, r)
+	})
+}
 
 func NewRouter() *mux.Router {
 	router := mux.NewRouter()
+	router.Use(loggingMiddleware)
 
 	router.HandleFunc("/{key}", PutHandler).Methods("PUT")
 	router.HandleFunc("/{key}", GetHandler).Methods("GET")
