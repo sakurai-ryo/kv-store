c1668f2592242c899ff6247bef7b14817b15abec
sakurai-ryo <sakurairyo.10do@gmail.com>
2021-12-25 13:50:05 +0900

    [add] delete lock

--- store.go
+++ store.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"github.com/cornelk/hashmap"
-	"sync"
 )
 
 type InMemoryStore struct {
-	sync.RWMutex
 	m *hashmap.HashMap
 }
 
@@ -15,33 +13,22 @@ var Store = InMemoryStore{
 }
 
 func Put(key string, value string) error {
-	Store.Lock()
-	defer Store.Unlock()
-
 	Store.m.Set(key, value)
 
 	return nil
 }
 
 func Get(key string) (string, error) {
-	Store.RLock()
-	defer Store.RUnlock()
-
 	value, ok := Store.m.Get(key)
-
 	if !ok {
 		return "", ErrorNoSuchKey
 	}
-
 	v := value.(string)
 
 	return v, nil
 }
 
 func Delete(key string) error {
-	Store.Lock()
-	defer Store.Unlock()
-
 	Store.m.Del(key)
 
 	return nil
